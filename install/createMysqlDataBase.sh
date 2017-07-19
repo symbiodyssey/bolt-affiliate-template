be06062ac9c43a23754ba7192eedd122a4616986
Joël <favre.joel@gmail.com>
2017-07-19 12:52:20 +0000

    Update createMysqlDataBase.sh

--- install/createMysqlDataBase.sh
+++ install/createMysqlDataBase.sh
@@ -4,11 +4,13 @@
 echo Hello, I'm Brian and I'm here to help you create a MySQL DataBase for Bolt CMS.
 echo I create DataBases since I'm 12 years old.
 echo Please let me know a few things about you.
+
 read -p 'What name did you choose for your new database ? ' dbname
 read -p 'What is the admin or root username ? ' mysqlroot
 read -sp 'What is the password ? ' rootpwd
 read -p 'What is the new username ? ' mysqluser
 read -sp 'What is the new password for this user ? ' mysqlpwd
+
 echo Thanks, that's should be very easy...
 
 #Create the new database and user for MySQL
