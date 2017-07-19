233cb9b770bc1482d413ada0a09263f8f8c66ead
Joël <favre.joel@gmail.com>
2017-07-19 14:05:16 +0000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,7 +8,7 @@ read -p 'What is the title of your website ? ' title
 echo Nice, I like it !
 
 read -p 'What is your domain name (without http and www, for instance : example.com) ? ' domain
-echo -e "\n\nOh wow ! I wish I\'ve thought about it before you"
+echo -e "\n\nOh wow ! I wish I've thought about it before you"
 echo -e "\n\n"
 read -p 'What is your username for MySQL ? ' mysqlroot
 read -sp 'What is the password of this user ? ' mysqlpwd
@@ -17,8 +17,9 @@ echo -e "\n\nThanks, now I know a new secret :P"
 echo But no worries you can trust me !
 
 echo -e "\n\nLet me ask you a last question"
-read -p "Where would you like to install Bolt, if you don\'t know, type /var/www ? " bolt_dir
+read -p "Where would you like to install Bolt, if you don't know, type /var/www ? " bolt_dir
 
+echo -e "\n\nThat shoud be enough, let me install it for you !"
 #Create directory with first parameter and go in
 mkdir $bolt_dir/$domain
 cd $bolt_dir/$domain
@@ -35,5 +36,14 @@ chown -R www-data:www-data extensions
 chown -R www-data:www-data public/files
 chown -R www-data:www-data public/thumbs
 
-echo -e "\n\n\nIt's all done !\nEnjoy  and see you !"
+mkdir tmp
+cd tmp
+
+curl -O https://github.com/symbiodyssey/bolt-affiliate-template/archive/master.tar.gz
+tar -xzf master.tar.gz --strip-components=1
+mv template ../public/theme/affiliatews
+cd ..
+rm -R tmp
+
+echo -e "\n\n\nIt's all done !\nEnjoy  and see you !\n\n"
 
