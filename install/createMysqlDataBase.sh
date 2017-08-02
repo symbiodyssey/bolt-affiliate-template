f7b1e3c2171f9c8f43095d68a7460544385b67f7
Joël <favre.joel@gmail.com>
2017-08-02 10:40:21 +0000

    Update createMysqlDataBase.sh

--- install/createMysqlDataBase.sh
+++ install/createMysqlDataBase.sh
@@ -9,13 +9,23 @@ read -p 'What name did you choose for your new database ? ' dbname
 read -p 'What is the admin or root username ? ' mysqlroot
 read -sp 'What is the password ? ' rootpwd
 
-read -p 'What is the new username ? ' mysqluser
-read -sp 'What is the new password for this user ? ' mysqlpwd
-
 echo Thanks, that\'s should be very easy...
+
+#Generates passwords
+function randpw { < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12};echo;}
+
+mysqluser='usr_$dbname'
+mysqlpwd=$(randpw);
 
 #Create the new database and user for MySQL
 mysql --user="$mysqlroot" --password="$rootpwd"  --execute="CREATE DATABASE $dbname character set UTF8 collate utf8_bin; CREATE USER '$mysqluser'@'localhost' IDENTIFIED BY '$mysqlpwd';GRANT ALL PRIVILEGES ON $dbname.* TO '$mysqluser'@'localhost';"
 
-echo It\'s all done !
-echo See you soon !
+echo -e "\n\n\n#####################################"
+echo -e "# Warning, please take note of that #"
+echo -e "#####################################"
+echo -e "One user has been created."
+echo -e "\tUsername: $mysqluser\n\tPassword: $mysqlpwd"
+echo -e "\n\nIt will not be shown a second time"
+echo -e "\nUse this user to connect Bolt to the database"
+
+echo -e "\n\n\nIt's all done !\nEnjoy  and see you !\n\n"
