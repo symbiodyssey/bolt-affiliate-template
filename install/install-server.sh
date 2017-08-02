7d835e8228e44427b3733826d56b0820987015d7
Joël <favre.joel@gmail.com>
2017-08-02 09:35:57 +0000

    Update install-server.sh

--- install/install-server.sh
+++ install/install-server.sh
@@ -2,7 +2,15 @@
 #Script to install the required softwares on Debian
 
 apt-get --assume-yes update
+systemctl stop apache2.service
+apt-get remove apache2
 apt-get --assume-yes install curl
+apt-get --assume-yes install nano
 apt-get --assume-yes install nginx
 apt-get --assume-yes install mysql-server
 apt-get --assume-yes install php5-fpm php5-mysql
+
+mysql_secure_installation
+
+sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
+systemctl restart php5-fpm
